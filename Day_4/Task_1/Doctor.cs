ad98a62e8359198f976aa1705aa701ec02dd8bca
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-04-12 23:01:18 +0530

    Doctor Task Completed

---
+++ Day_4/Task_1/Doctor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Doctor
+    {
+        /// <summary>
+        /// creating a id (Integer) and Getting it up
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Creating a name (String) and Getting it up
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Creating a Age(Integer) and Getting it up
+        /// </summary>
+        public int Age { get; private set; }
+
+        /// <summary>
+        /// Creating a Experience(Integer) and Getting it up
+        /// </summary>
+        public int Experience { get; private set; }
+        /// <summary>
+        /// Creating a Qualification(String) and Getting it up
+        /// </summary>
+        public string Qualification { get; private set; }
+        /// <summary>
+        /// Creating a Speciality(String) and Getting it up
+        /// </summary>
+        public string Speciality { get; private set; }
+
+        /// <summary>
+        /// Default Constructor for the Class : Doctor
+        /// </summary>
+        public Doctor()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Age = 0;
+            Experience = 0;
+            Qualification = string.Empty;
+            Speciality = string.Empty;
+        }
+        /// <summary>
+        /// Parameterized Constructor for Class : Doctor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <param name="experience"></param>
+        /// <param name="qualification"></param>
+        /// <param name="speciality"></param>
+        public Doctor(int id, string name, int age , int experience, string qualification, string speciality)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Experience = experience;
+            Qualification = qualification;
+            Speciality = speciality;
+        }
+        
+        public void PrintingDoctorDetails()
+        {
+            Console.WriteLine($"Id : {Id} \n Name : {Name} \n Age : {Age} \n Experience : {Experience} \n Qualification : {Qualification} \n Speciality : {Speciality} \n");
+        }
+
+
+    }
+}
---
+++ Day_4/Task_1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Transactions;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Handling Input , Exception for Integer feild
+        /// </summary>
+        /// <returns>Integer</returns>
+        int HandlingIntegerInput()
+        {
+            int num = 0;
+            while(int.TryParse(Console.ReadLine(),out num) == false)
+            {
+                Console.WriteLine("Please Enter a valid number \n");
+            }
+            return num;
+
+        }
+        /// <summary>
+        /// Handling Input , Exception for String feild
+        /// </summary>
+        /// <returns>String</returns>
+        string HandlingStringInput()
+        {
+            string ? str  =string.Empty;
+            do
+            {
+                str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(str))
+                Console.WriteLine("Please Enter a valid string \n");
+            } while (string.IsNullOrWhiteSpace(str));
+            return str;
+
+        }
+         /// <summary>
+         /// Takes Counter (Integer) as Argument and Creats Doctor 
+         /// </summary>
+         /// <param name="counter"></param>
+         /// <returns>Doctor class</returns>
+        Doctor CreateDoctor(int counter) {
+            int id = int.Parse((DateTime.Now.ToString("yyyyMMdd"))),age ,experience;
+            string name, qualification, speciality;
+            Console.WriteLine($"Please Enter the Age of {counter + 1} doctor : ");
+            age = HandlingIntegerInput();
+            Console.WriteLine($"Please Enter the Experience of {counter + 1} doctor : ");
+            experience = HandlingIntegerInput();
+            Console.WriteLine($"Please Enter the Name of {counter + 1} doctor : ");
+            name = HandlingStringInput();
+            Console.WriteLine($"Please Enter the Qualification of {counter + 1} doctor : ");
+            qualification = HandlingStringInput();
+            Console.WriteLine($"Please Enter the Speciality of {counter + 1} doctor : ");
+            speciality = HandlingStringInput();
+
+            Doctor doctor = new Doctor(id,name,age,experience,qualification,speciality);
+            return doctor;
+        }
+        /// <summary>
+        /// Takes Specailty (String) , Doctor (Array)
+        /// </summary>
+        /// <param name="specailty"></param>
+        /// <param name="doctors"></param>
+      static void GetDoctorBySpecailty(string specailty , Doctor[] doctors)
+         {
+            bool flag = false;
+            for(int i = 0; i < doctors.Length; i++)
+            {
+                if (doctors[i].Speciality.ToLower() == specailty.ToLower())
+                {
+                    flag = true;
+                    doctors[i].PrintingDoctorDetails();
+                    break;
+                }
+            }
+            if(!flag) { Console.WriteLine("There Are No Such Doctors With Given Speciality"); }
+            return;
+         }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Please Enter the number of doctors : ");
+            //Console.WriteLine(DateTime.Now.ToString("yyyyMMdd"));
+            int size = program.HandlingIntegerInput();
+            Doctor[] doctors = new Doctor[size];
+            Console.WriteLine("_________________________Creating Doctor _____________________________________");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("************************************************* \n");
+                Console.WriteLine($"Please Enter Doctor {i+1} Details \n");
+                doctors[i] = program.CreateDoctor(i);
+               
+            }
+            Console.WriteLine("_________________________Fetching All Doctors _____________________________________");
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"The Doctor {i + 1} Details \n");
+                doctors[i].PrintingDoctorDetails();
+            }
+            Console.WriteLine("_________________________Search Doctor by Speciality_____________________________________");
+            string speciality;
+            do
+            {
+                Console.WriteLine("Please Enter the Speciality of Doctor or Enter q for quit: ");
+                speciality = program.HandlingStringInput();
+                if(speciality != "q") GetDoctorBySpecailty(speciality, doctors);
+            } while (speciality != "q");
+
+        }
+    }
+}
