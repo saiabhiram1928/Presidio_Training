61b4c9c32ce55f720a793ce249e6a50514fdb738
SaiAbhiRam <saiabhiram1928@gamil.com>
2024-04-12 13:50:53 +0530

    Day 3 morning class practice

---
+++ Day 3/first_cli_installtion/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace first_cli_installtion{
+    internal class Program{
+        static void Main(string[] args){
+            int num1 =Convert.ToInt32 (Console.ReadLine());
+            float fnum2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine($"The float value {fnum2}");
+
+            Console.WriteLine("hello world");
+        }
+    }
+}
