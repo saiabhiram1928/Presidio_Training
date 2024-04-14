9fd2157107c77e31d5346274df09ed4063cbd7a2
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-04-14 15:19:35 +0530

    Task_3

---
+++ Day_3/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static float Average()
+        {
+            int num = 0, sum = 0, count = 0;
+            do
+            {
+                Console.WriteLine("Enter the number (or) Enter -1 to quit : ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0) break;
+                if(num % 7 == 0)
+                {
+                    sum += num;
+                    count++;
+                }
+            } while (num > 0);
+            if (num < 0 && sum == 0 && count == 0) return -1;
+            return (float)sum / (float) count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Welcome--------------");
+          float avg = Average();
+            if(avg > 0) 
+                 Console.WriteLine($"The average of numbers divided by 7 for the given numbers is : {avg}");
+        }
+    }
+}
