0690b7cc7eb336222140d0900af031cb9ec919b8
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-04-15 17:00:11 +0530

    Day_5:CowBull Game and Update,Delete Opt Taks Completed

---
+++ Day_5/ReqTrackerSolution/ReqTrackerApplication/Program.cs
@@ -0,0 +1,236 @@
+﻿using ReqTrackerModellib;
+using System.Xml.Linq;
+
+namespace ReqTrackerApplication
+{
+    internal class Program
+    {
+        void UnderstandingSqueceExecution()
+        {
+            Console.WriteLine("Helloo Welcome");
+            Console.WriteLine("my name is C#");
+            int num1 =1, num2 =2;
+            Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+        }
+        void UndersttandingCondtionlStatement()
+        {
+            string str = "ramu";
+            if(str  == "ramu") Console.WriteLine("Hii Ramu");
+            else if(str == "somu") Console.WriteLine("Hii Somu");
+            else Console.WriteLine("Hiii unknown");
+        }
+        void UnderstandingSwitchCase()
+        {
+            string str = "ramu";
+            switch(str)
+            {
+                case "ramu":
+                    Console.WriteLine("Hii ramu");
+                    break;
+                case "somu":
+                    Console.WriteLine("Hii somu");
+                    break;
+                default:
+                    Console.WriteLine("Hii unknown");
+                    break;
+            }
+        }
+        bool PerformValidation(int num)
+        {
+            int prev = num % 10;
+            num /= 10;
+            while(num > 0)
+            {
+                int digit = num % 10;
+                if (prev != digit) return false;
+                prev = digit;
+                num /= 10;
+            }
+            return  true;
+
+        }
+        void UnderStandingArrays()
+        {
+            int[] arr = { 666, 777, 121 , 111 ,100 } ;
+            int ans = 0;
+            for(int i =0; i<arr.Length ; i++)
+            {
+                if (PerformValidation(arr[i])) ans += 1;
+            }
+            Console.WriteLine("The count of number having same digtis is  : " + ans);
+        }
+        Employee[] employees;
+        public Program()
+        {
+            employees = new Employee[3];
+        }
+        void PrintMenu()
+        {
+            Console.WriteLine("1. Add Employee");
+            Console.WriteLine("2. Print Employees");
+            Console.WriteLine("3. Update Employee");
+            Console.WriteLine("4. Delete Employee");
+            Console.WriteLine("0. Exit");
+        }
+        void EmployeeInteraction()
+        {
+            int choice = 0;
+            do
+            {
+                PrintMenu();
+                Console.WriteLine("Please select an option");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Bye.....");
+                        break;
+                    case 1:
+                        AddEmployee();
+                        break;
+                    case 2:
+                        PrintAllEmployees();
+                        break;
+                    case 3:
+                        UpdateEmployee();
+                        break;
+                    case 4:
+                        DeleteEmployee();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again");
+                        break;
+                }
+            } while (choice != 0);
+        }
+        void AddEmployee()
+        {
+            if (employees[employees.Length - 1] != null)
+            {
+                Console.WriteLine("Sorry we have reached the maximum number of employees");
+                return;
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i] == null)
+                {
+                    employees[i] = CreateEmployee(i);
+                }
+            }
+
+        }
+        void PrintAllEmployees()
+        {
+            if (employees[employees.Length-1] == null)
+            {
+                Console.WriteLine("No Employees available");
+                return;
+            }
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i] != null)
+                    PrintEmployee(employees[i]);
+            }
+        }
+        Employee CreateEmployee(int id)
+        {
+            Employee employee = new Employee();
+            employee.Id = 101 + id;
+            employee.BuildEmployeeFromConsole();
+            return employee;
+        }
+
+        void PrintEmployee(Employee employee)
+        {
+            Console.WriteLine("---------------------------");
+            employee.PrintEmployeeDetails();
+            Console.WriteLine("---------------------------");
+        }
+        int HandlingIntegerInput()
+        {
+            int num = 0;
+            while (int.TryParse(Console.ReadLine(), out num) == false)
+            {
+                Console.WriteLine("Please Enter a valid number \n");
+            }
+            return num;
+
+        }
+        string HandlingStringInput()
+        {
+            return Console.ReadLine() ?? String.Empty;
+        }
+        int GetIdFromConsole()
+        {
+            return HandlingIntegerInput();
+        }
+        void GetEmployeeByIdAndUpdate(int id)
+        {
+             
+            for(int i =0;i<employees.Length; i++)
+            {
+                if (employees[i] != null && employees[i].Id == id)
+                {
+                    Console.WriteLine("Please Enter Updated Name : ");
+                    employees[i].Name = HandlingStringInput();
+                    PrintEmployee(employees[i]);
+                    return;
+                }
+            }
+            Console.WriteLine("There is no such employee with given id");
+            return;
+        }
+        void UpdateEmployee()
+        {
+            int id;
+            if (employees[0] == null)
+            {
+                Console.WriteLine("No Employee Available");
+                return;
+            }
+            Console.WriteLine("Please Enter Id of the employee to be updated : ");
+            id = GetIdFromConsole();
+             GetEmployeeByIdAndUpdate(id);
+            return;
+        }
+        void GetEmployeeByIdAndDelete(int id)
+        {
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (employees[i] != null && employees[i].Id == id)
+                {
+                    Console.WriteLine("Please Enter Updated Name : ");
+                    employees[i]  = null;
+                    return;
+                }
+            }
+            Console.WriteLine("There is no such employee with given id");
+            return;
+        }
+        void DeleteEmployee()
+        {
+            int id;
+            if (employees[0] == null)
+            {
+                Console.WriteLine("No Employee Available");
+                return;
+            }
+            Console.WriteLine("Please Enter Id of the employee to be deleted : ");
+            id = GetIdFromConsole();
+            GetEmployeeByIdAndDelete(id);
+            PrintAllEmployees();
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //program.UnderstandingSqueceExecution();
+            //program.UndersttandingCondtionlStatement();
+            //program.UnderstandingSwitchCase();
+            //program.UnderStandingArrays();
+            program.EmployeeInteraction();
+        }
+    }
+}
---
+++ Day_5/morning_class/CowBullSolution/CowBullApplication/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CowBullApplication
+{
+    internal class Program
+    {
+        string HandlingStringInput()
+        {
+            string str = string.Empty;
+            do
+            {
+                str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(str) || str?.Length !=4)
+                    Console.WriteLine("Please Enter a valid string \n");
+            } while (string.IsNullOrWhiteSpace(str) || str.Length != 4);
+            return str;
+
+        }
+        int[] CountOfCowsAndBulls(string str,  string main)
+        {
+            int[] cowandbull = new int[2];
+            char[] temp = (main.ToCharArray());
+            for (int i = 0; i < 4; i++) 
+            {
+               
+                if (str[i] == main[i]) {cowandbull[0]++ ; temp[i] = '#';  continue; }
+                int index = Array.IndexOf(temp, str[i]);
+                if (index > 0)
+                {
+                    temp[index] = '#';
+                    Console.WriteLine(temp);
+                    cowandbull[1]++;
+                }
+            }
+            return cowandbull;
+        }
+        void CowAndBullGame()
+        {
+            Console.WriteLine("Please Enter Main Word : ");
+            string main = HandlingStringInput(),str;
+            do 
+            {
+                Console.WriteLine("Please Enter the Guess Word or -1 to exit: ");
+                str = HandlingStringInput();
+                int[] CowAndBull = CountOfCowsAndBulls(str, main);
+                if (CowAndBull[0] != 4)
+                   Console.WriteLine($"Cow : {CowAndBull[0]} , Bull : {CowAndBull[1]}");
+                else
+                {
+
+                    Console.WriteLine("Congrate You Won");
+                    break;
+                }
+            } while (str != "-1" );
+            return;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.CowAndBullGame();
+         
+        }
+    }
+}
