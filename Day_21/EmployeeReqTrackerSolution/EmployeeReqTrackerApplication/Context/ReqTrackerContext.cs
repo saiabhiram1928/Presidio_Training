74c2d9e982c1ca4c6dc35ab5d6d7f5e11e83be38
saiabhiram1928 <saiabhiram1928@gmail.com>
2024-05-16 08:15:38 +0530

    Day_21 Class Completed

---
+++ Day_21/EmployeeReqTrackerSolution/EmployeeReqTrackerApplication/Context/ReqTrackerContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeReqTrackerApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReqTrackerApplication.Context
+{
+    public class ReqTrackerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=8RXBBX3\\PRACTICEINSTANCE;Integrated Security=true;Initial Catalog=db_EmployeeReqTracker");
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ Day_21/EmployeeReqTrackerSolution/EmployeeReqTrackerApplication/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeReqTrackerApplication.Models
+{
+    public class Employee
+    {
+        [Key]
+       public int Id { get; set; }
+       public string Name { get; set; } = string.Empty;
+       public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
